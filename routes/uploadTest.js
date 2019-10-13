9ab3e458e2b40f4a4bda49e0758b29d496c3d1a0
Ruben Green <ragrcg@gmail.com>
2019-10-13 15:25:37 -0700

    added uploads directory

--- routes/uploadTest.js
+++ routes/uploadTest.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET file upload page. */
 router.get('/', function(req, res, next) {
   var options = {
   	root: __dirname + '/../views/'
   }
   res.sendFile('uploadTest.html', options);
 });
+
+router.get('/getFile', function(req, res, next) {
+	var options = {
+  		root: __dirname + '/../uploads/'
+  	}
+  	res.sendFile('testImage.png', options);
+})
 
 module.exports = router;
